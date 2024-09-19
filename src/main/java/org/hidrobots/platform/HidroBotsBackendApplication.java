6043b14ffd4c36474b43f6e12e735b0c2af3af07
IsaacAmp24 <ampudiaflores@gmail.com>
2024-09-18 23:23:59 -0500

    Initial Commit.

---
+++ src/main/java/org/hidrobots/platform/HidroBotsBackendApplication.java
@@ -0,0 +1,13 @@
+package org.hidrobots.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HidroBotsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HidroBotsBackendApplication.class, args);
+    }
+
+}
