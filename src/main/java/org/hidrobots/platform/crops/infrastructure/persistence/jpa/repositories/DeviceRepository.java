e877d341b523d226be177c45a8a98b7ce596662f
TMedalith <u20211f955@upc.edu.pe>
2024-11-14 23:17:55 -0500

    feat(device): Add Device entity and repository

--- src/main/java/org/hidrobots/platform/crops/infrastructure/persistence/jpa/repositories/DeviceRepository.java
+++
@@ -1,8 +0,0 @@
-package org.hidrobots.platform.crops.infrastructure.persistence.jpa.repositories;
-
-
-import org.hidrobots.platform.crops.domain.model.entities.Device;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeviceRepository extends JpaRepository<Device, Long> {
-}
---
+++ src/main/java/org/hidrobots/platform/devices/infrastructure/persistence/jpa/repositories/DeviceRepository.java
@@ -0,0 +1,18 @@
+package org.hidrobots.platform.devices.infrastructure.persistence.jpa.repositories;
+
+import org.hidrobots.platform.devices.domain.model.entities.Device;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DeviceRepository extends JpaRepository<Device, Long> {
+    List<Device> findByCropId(Long cropId);
+
+    @Query("SELECT d FROM Device d WHERE d.cropId IN (SELECT c.id FROM Crop c WHERE c.farmerId = :farmerId)")
+    List<Device> findByFarmerId(@Param("farmerId") Long farmerId);
+
+}
