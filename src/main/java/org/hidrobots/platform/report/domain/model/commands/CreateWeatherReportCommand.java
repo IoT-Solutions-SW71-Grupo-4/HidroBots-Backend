bd97687a84bd9969a3571b2a073473974928e8a6
TMedalith <u20211f955@upc.edu.pe>
2024-11-14 23:16:18 -0500

    feat(device): Add device command implementations

--- src/main/java/org/hidrobots/platform/report/domain/model/commands/CreateWeatherReportCommand.java
+++ src/main/java/org/hidrobots/platform/report/domain/model/commands/CreateWeatherReportCommand.java
@@ -1,6 +1,5 @@
 package org.hidrobots.platform.report.domain.model.commands;
 
-import org.hidrobots.platform.crops.domain.model.entities.Device;
 
 public record CreateWeatherReportCommand(
         Double temperature,
