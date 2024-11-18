d9c535c72d7f0b969aa7fa75e45f9adaaec6bb5c
Diego Defilippi <defsan35@gmail.com>
2024-11-18 12:36:10 -0500

    feat(cors): Allow PATCH method in CORS configuration.

--- src/main/java/org/hidrobots/platform/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
+++ src/main/java/org/hidrobots/platform/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
@@ -66,7 +66,7 @@ public class WebSecurityConfiguration {
         http.cors(configurer -> configurer.configurationSource(source -> {
             var cors = new CorsConfiguration();
             cors.setAllowedOrigins(List.of("*"));
-            cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+            cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
             cors.setAllowedHeaders(List.of("*"));
             return cors;
         }));
